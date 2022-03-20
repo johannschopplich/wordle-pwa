6bf3ed6348d43cf6fb0a82302bfabeeceb939960
Johann Schopplich <mail@johannschopplich.com>
2022-03-21 00:46:04 +0100

    types: mark custom env options as optional

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,6 @@ declare module "~icons/*" {
 }
 
 interface ImportMetaEnv {
-  readonly VITE_STARTS_AT: string;
-  readonly VITE_ANSWERS: string;
+  readonly VITE_STARTS_AT?: string;
+  readonly VITE_ANSWERS?: string;
 }
