5b4f1544475708773aed9bc1469e29619ae98832
Johann Schopplich <mail@johannschopplich.com>
2022-03-21 15:13:13 +0100

    chore: use unplugin icons virtual import env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,3 @@
-declare module "~icons/*" {
-  import type { FunctionalComponent } from "vue";
-  const component: FunctionalComponent;
-  export default component;
-}
-
 interface ImportMetaEnv {
   readonly VITE_STARTS_AT?: string;
   readonly VITE_ANSWERS?: string;
