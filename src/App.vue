6bbc5f03e0a9a3eaa1ae70740a06aa91f725cc00
Johann Schopplich <mail@johannschopplich.com>
2022-03-22 21:23:15 +0100

    fix: store tomorrow in app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <script setup lang="ts">
-import { until, useNow } from "@vueuse/core";
+import { until, useNow, useStorage } from "@vueuse/core";
 
 const now = useNow();
-let tomorrow = new Date();
-tomorrow.setDate(now.value.getDate() + 1);
+const tomorrow = useStorage<Date>(
+  "app.lastMatchDay",
+  getTomorrow(),
+  undefined,
+  {
+    serializer: {
+      read: (v: any) => new Date(v),
+      write: (v: any) => v.toISOString(),
+    },
+  }
+);
+
+if (tomorrow.value.getTime() < now.value.getTime()) {
+  reset();
+}
 
 // Reset the app when the next day has dawned
-(async () => {
-  await until(now).toMatch((v) => v.getTime() > tomorrow.getTime());
-  localStorage.clear();
-  location.reload();
-})();
+until(now)
+  .toMatch((v) => v.getTime() > tomorrow.value.getTime())
+  .then(reset);
+
+let boardKey = $ref(0);
+
+function getTomorrow() {
+  return new Date(
+    now.value.getFullYear(),
+    now.value.getMonth(),
+    now.value.getDate() + 1
+  );
+}
+
+function reset() {
+  tomorrow.value = getTomorrow();
+  // Reset board state
+  localStorage.removeItem("app.board");
+  boardKey++;
+}
 </script>
 
 <template>
   <div class="h-full grid grid-rows-[auto_1fr_auto] gap-4 children:min-w-0">
     <Header />
 
-    <Board />
+    <Board :key="boardKey" />
   </div>
 </template>
