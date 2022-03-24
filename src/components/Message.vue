750e38e0f59639c9553f40c0fe211a4557539cdd
Johann Schopplich <mail@johannschopplich.com>
2022-03-24 19:07:27 +0100

    refactor: success message modal styles

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -9,7 +9,7 @@ defineProps<{
   <Teleport to="body">
     <div
       v-if="isOpen && size === 'large'"
-      class="absolute inset-0 bg-gray-800 bg-opacity-50 translate-z-1 z-20 dark:bg-zinc-800 dark:bg-opacity-50"
+      class="absolute inset-0 bg-neutral-800 bg-opacity-50 translate-z-1 z-20"
     />
 
     <Transition>
@@ -17,10 +17,11 @@ defineProps<{
         v-if="isOpen"
         :class="[
           'message',
-          'absolute top-23 left-1/2 shadow-lg transition-opacity-250 -translate-x-1/2 translate-z-1 z-20 dark:text-gray-800',
+          'absolute top-23 left-1/2 shadow-sm transition-opacity-250 -translate-x-1/2 translate-z-1 z-20',
           size === 'default' &&
-            'bg-gray-800 text-white px-4 py-2 rounded-full dark:bg-white',
-          size === 'large' && 'bg-white p-6 rounded-xl',
+            'bg-gray-800 text-white px-4 py-2 rounded-full dark:bg-white dark:text-gray-800',
+          size === 'large' &&
+            'bg-white text-gray-800 p-6 rounded-xl border-2 border-amber-800',
         ]"
       >
         <div class="space-y-6 text-center">
