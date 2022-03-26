41abc31bb1f42c6641655f9c77fc79fc6b54588e
Johann Schopplich <mail@johannschopplich.com>
2022-03-26 17:31:21 +0100

    fix: debounced resize to fix iOS Safari bug in PWA mode

--- src/modules/head.ts
+++ src/modules/head.ts
@@ -2,15 +2,19 @@ import type { UserModule } from "~/types";
 
 export const install: UserModule = () => {
   // Set actual vh on mobile
-  const onResize = () =>
+  const setVh = () =>
     document.documentElement.style.setProperty(
       "--vh",
       `${window.innerHeight * 0.01}px`
     );
 
-  // Resize for scaling the board size
-  window.addEventListener("resize", onResize);
+  window.addEventListener("resize", () => {
+    setVh();
+
+    // Debounced resize to fix iOS Safari bug in PWA mode
+    setTimeout(setVh, 100);
+  });
 
   // Set size on startup
-  onResize();
+  setVh();
 };
