a472c53e477282f6724ef1400aff1860488d5dd9
Johann Schopplich <mail@johannschopplich.com>
2022-11-28 14:09:25 +0100

    chore: lint Tailwind class order in UnoCSS config

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, presetTypography, presetWind } from "unocss";
 import { theme } from "@unocss/preset-wind";
 
 const control =
-  "appearance-none text-base leading-4 px-3 py-2 border rounded-md shadow-sm focus:outline-none sm:text-sm";
+  "appearance-none rounded-md border px-3 py-2 text-base leading-4 shadow-sm focus:outline-none sm:text-sm";
 
 export default defineConfig({
   theme: {
@@ -22,16 +22,16 @@ export default defineConfig({
       },
     },
     fontFamily: {
-      heading: `Henrietta,${theme.fontFamily.sans}`,
-      "heading-condensed": `Henrietta Condensed,${theme.fontFamily.sans}`,
+      heading: `Henrietta,${theme.fontFamily!.sans}`,
+      "heading-condensed": `Henrietta Condensed,${theme.fontFamily!.sans}`,
     },
   },
   shortcuts: {
-    box: "bg-white rounded-md shadow-sm p-4 dark:bg-gray-800",
-    button: `${control} inline-flex items-center justify-center border-transparent bg-primary-600 text-white font-500 whitespace-nowrap hover:bg-primary-700 focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 disabled:cursor-default disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-100 disabled:hover:bg-gray-100 dark:focus:ring-offset-gray-900`,
-    input: `${control} w-full bg-white border-gray-300 placeholder-gray-400 focus:border-primary-600 focus:ring focus:ring-primary-600 disabled:bg-gray-100 disabled:border-gray-100 disabled:text-gray-500 dark:bg-gray-800 dark:border-gray-500`,
+    box: "rounded-md bg-white p-4 shadow-sm dark:bg-gray-800",
+    button: `${control} bg-primary-600 font-500 hover:bg-primary-700 focus:ring-primary-600 inline-flex items-center justify-center whitespace-nowrap border-transparent text-white focus:ring-2 focus:ring-offset-2 disabled:cursor-default disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-500 disabled:hover:bg-gray-100 dark:focus:ring-offset-gray-900`,
+    input: `${control} focus:border-primary-600 focus:ring-primary-600 w-full border-gray-300 bg-white placeholder-gray-400 focus:ring disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-500 dark:border-gray-500 dark:bg-gray-800`,
     "icon-inline":
-      "w-[1em] h-[1em] relative bottom-[0.125em] inline-block fill-current text-current select-none",
+      "relative bottom-[0.125em] inline-block h-[1em] w-[1em] select-none fill-current text-current",
   },
   presets: [presetWind(), presetTypography()],
 });
