3f09d52445a006ade92a0c89c36f54cfc0fa8226
Johann Schopplich <mail@johannschopplich.com>
2022-12-14 20:54:24 +0100

    chore: simplify locale loading

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,13 +1,20 @@
 import { createI18n } from "@leanera/vue-i18n";
-import de from "~/locales/de.json";
 import type { UserModule } from "~/types";
 
 export const install: UserModule = ({ app }) => {
+  // Auto-load translations
+  const messages = Object.fromEntries(
+    Object.entries(
+      import.meta.glob<Record<string, any>>("../locales/*.json", {
+        eager: true,
+      })
+    ).map(([key, value]) => [key.slice(11, -5), value])
+  );
+
   const i18n = createI18n({
     defaultLocale: "de",
-    messages: {
-      de,
-    },
+    locales: Object.keys(messages),
+    messages,
   });
 
   app.use(i18n);
