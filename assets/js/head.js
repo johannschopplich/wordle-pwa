1c1a049f924394b72d3e04e11450fe9e507ba38a
Johann Schopplich <mail@johannschopplich.com>
2023-08-22 11:54:30 +0200

    chore: enable dark mode only on mobile by default

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,7 +1,11 @@
 ;((root) => {
   const prefersDark = matchMedia('(prefers-color-scheme: dark)').matches
+  const isMobile = matchMedia('(hover: none)').matches
   const themeSetting = localStorage.getItem('app.color-scheme')
-  if (themeSetting === 'dark' || (prefersDark && themeSetting !== 'light')) {
+  if (
+    themeSetting === 'dark' ||
+    (isMobile && prefersDark && themeSetting !== 'light')
+  ) {
     root.classList.toggle('dark', true)
   }
 })(document.documentElement)
