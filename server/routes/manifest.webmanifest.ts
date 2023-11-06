65b66e9f915184504a73b7f1635e7f5bf64c2b13
Johann Schopplich <mail@johannschopplich.com>
2023-11-06 13:52:26 +0100

    refactor: simplify webmanifest

--- server/routes/manifest.webmanifest.ts
+++ server/routes/manifest.webmanifest.ts
@@ -46,6 +46,12 @@ export default defineEventHandler(async () => {
         sizes: '512x512',
         type: 'image/png',
       },
+      {
+        src: '/images/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
     ],
   }
 })
